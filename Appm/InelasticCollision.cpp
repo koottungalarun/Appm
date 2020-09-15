4a0eed78ead8ef0bdae843da1a238b7f0d475f56
Roman Fuchs <roman.fuchs@hotmail.com>
2020-09-15 09:22:48 +0200

    add functions that implement coefficients in Le and Cambier (2016)

--- Appm/InelasticCollision.cpp
+++ Appm/InelasticCollision.cpp
@@ -5,7 +5,9 @@ InelasticCollision::InelasticCollision()
 {
 }
 
-InelasticCollision::InelasticCollision(const std::string & folderPath, const int idxE_, const int idxA_, const int idxI_) 
+InelasticCollision::InelasticCollision(const std::string & folderPath, 
+	const int idxE_, const int idxA_, const int idxI_, 
+	const ScalingParameters & scales) 
 	: idxE(idxE_), idxA(idxA_), idxI(idxI_)
 {
 	const DataTransform xTrans = DataTransform::NONE;
@@ -43,10 +45,121 @@ InelasticCollision::InelasticCollision(const std::string & folderPath, const int
 
 	filename = folderPath + "I_R2rec.csv";
 	data_R2rec = Interpolation2d(filename, xTrans, yTrans, fTrans);
+
+	// set parameters 
+	const double ionizationEnergy_eV = 15.75;
+	this->ionizationEnergyScaled = Physics::electronVolt_to_Joule(ionizationEnergy_eV) / scales.getEnergyScale();
+	this->nScale = scales.getNumberDensityScale();
+	this->g0 = 1;
+	this->g1 = 6;
+
+	PhysicsConstants & pc = PhysicsConstants::instance();
+	const double pi = pc.pi();
+	const double kB = pc.kB();
+	const double h = pc.planckConstant();
+	const double massScale = scales.getMassScale();
+	const double Tscale = scales.getTemperatureScale();
+	this->thermalDeBroglieScale = h / sqrt(2*pi* massScale * kB * Tscale);
+	assert(std::isfinite(thermalDeBroglieScale));
+	assert(thermalDeBroglieScale > 0);
 }
 
 InelasticCollision::~InelasticCollision()
 {
+}
+
+const Eigen::VectorXd InelasticCollision::getGion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & lambdaIon, const Eigen::VectorXd & TeVec) const
+{	
+	const Eigen::VectorXd I_Gion = getGionInterpolated(TeVec);
+	const Eigen::VectorXd Gion = nE.array() * nA.array() * vthE.array() * (-1 * lambdaIon.array()).exp() * I_Gion.array();
+	assert(Gion.allFinite());
+	return Gion;
+}
+
+const Eigen::VectorXd InelasticCollision::getGrec(const double mE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & nI, const Eigen::VectorXd & nE, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & Te, const Eigen::VectorXd & lambdaVec) const
+{
+	const double factor0 = nScale * pow(thermalDeBroglieScale, 3) * g0 / (2 * g1);
+	const Eigen::VectorXd factor1 = (mE * TeVec).array().pow(-3./2.) * nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaVec).array().exp() * xStar.array().exp();
+	const Eigen::VectorXd I_Grec = getGrecInterpolated(Te, lambdaVec);
+	const Eigen::VectorXd Grec = factor0 * factor1.array() * factor2.array() * I_Grec.array();
+	assert(Grec.allFinite());
+	return Grec;
+}
+
+const Eigen::VectorXd InelasticCollision::getR0ion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaIon) const
+{
+	const Eigen::VectorXd I_R0ion = getR0ionInterpolated(TeVec, lambdaIon);
+	const Eigen::VectorXd factor0 = 2./3. * nE.array() * nA.array() * vthE.array();
+	const Eigen::VectorXd factor1 = (-1 * lambdaIon).array().exp() * I_R0ion.array();
+	const Eigen::VectorXd R0ion = factor0.array() * factor1.array();
+	assert(R0ion.allFinite());
+	return R0ion;
+}
+
+const Eigen::VectorXd InelasticCollision::getJ00ion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaIon) const
+{
+	const Eigen::VectorXd I_J00ion = getJ00ionInterpolated(TeVec, lambdaIon);
+	const Eigen::VectorXd factor0 = 2. / 3. * nE.array() * nA.array() * vthE.array();
+	const Eigen::VectorXd factor1 = (-1 * lambdaIon).array().exp() * I_J00ion.array();
+	const Eigen::VectorXd J00ion = factor0.array() * factor1.array();
+	assert(J00ion.allFinite());
+	return J00ion;
+}
+
+const Eigen::VectorXd InelasticCollision::getR1rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const
+{
+	const Eigen::VectorXd I_R1rec = getR1recInterpolated(TeVec, lambdaRec);
+	const double factor0 = 2. / 3. * (0.5 * g0) / g1 * nScale * pow(thermalDeBroglieScale, 3);
+	const Eigen::VectorXd factor1 = nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaRec).exp() * xStar.array().exp() * I_R1rec;
+	const Eigen::VectorXd R1rec = factor0 * factor1.array() * factor2.array();
+	assert(R1rec.allFinite());
+	return R1rec;
+}
+
+const Eigen::VectorXd InelasticCollision::getR2rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const
+{
+	const Eigen::VectorXd I_R2rec = getR2recInterpolated(TeVec, lambdaRec);
+	const double factor0 = 2. / 3. * (0.5 * g0) / g1 * nScale * pow(thermalDeBroglieScale, 3);
+	const Eigen::VectorXd factor1 = nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaRec).exp() * xStar.array().exp() * I_R2rec;
+	const Eigen::VectorXd R2rec = factor0 * factor1.array() * factor2.array();
+	assert(R2rec.allFinite());
+	return R2rec;
+}
+
+const Eigen::VectorXd InelasticCollision::getJ11rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const
+{
+	const Eigen::VectorXd I_J11rec = getJ11recInterpolated(TeVec, lambdaRec);
+	const double factor0 = (0.5 * g0) / g1 * nScale * pow(thermalDeBroglieScale, 3);
+	const Eigen::VectorXd factor1 = nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaRec).exp() * xStar.array().exp() * I_J11rec;
+	const Eigen::VectorXd R2rec = factor0 * factor1.array() * factor2.array();
+	assert(R2rec.allFinite());
+	return R2rec;
+}
+
+const Eigen::VectorXd InelasticCollision::getJ22rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const
+{
+	const Eigen::VectorXd I_J22rec = getJ22recInterpolated(TeVec, lambdaRec);
+	const double factor0 = (0.5 * g0) / g1 * nScale * pow(thermalDeBroglieScale, 3);
+	const Eigen::VectorXd factor1 = nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaRec).exp() * xStar.array().exp() * I_J22rec;
+	const Eigen::VectorXd R2rec = factor0 * factor1.array() * factor2.array();
+	assert(R2rec.allFinite());
+	return R2rec;
+}
+
+const Eigen::VectorXd InelasticCollision::getJ12rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const
+{
+	const Eigen::VectorXd I_J12rec = getJ12recInterpolated(TeVec, lambdaRec);
+	const double factor0 = 4./9. * (0.5 * g0) / g1 * nScale * pow(thermalDeBroglieScale, 3);
+	const Eigen::VectorXd factor1 = nI.array() * nE.array().pow(2) * vthE.array();
+	const Eigen::VectorXd factor2 = (-2 * lambdaRec).exp() * xStar.array().exp() * lambdaRec.array() * I_J12rec;
+	const Eigen::VectorXd R2rec = factor0 * factor1.array() * factor2.array();
+	assert(R2rec.allFinite());
+	return R2rec;
 }
 
 const Eigen::VectorXd InelasticCollision::getGionInterpolated(const Eigen::VectorXd & Te) const
@@ -107,5 +220,10 @@ const int InelasticCollision::getIonFluidx() const
 const int InelasticCollision::getAtomFluidx() const
 {
 	return this->idxA;
+}
+
+const double InelasticCollision::getIonizationEnergyScaled() const
+{
+	return this->ionizationEnergyScaled;
 }
 
--- Appm/InelasticCollision.h
+++ Appm/InelasticCollision.h
@@ -6,6 +6,9 @@
 #include "Interpolation1d.h"
 #include "Interpolation2d.h"
 #include "DataTransform.h"
+#include "Physics.h"
+#include "PhysicsConstants.h"
+#include "ScalingParameters.h"
 #include <fstream>
 #include <Eigen/Dense>
 
@@ -15,28 +18,39 @@ public:
 	InelasticCollision();
 
 	/* Read data for inelastic collisions from folder */
-	InelasticCollision(const std::string & folderPath, const int idxE, const int idxA, const int idxI);
+	InelasticCollision(const std::string & folderPath, 
+		const int idxE_, const int idxA_, const int idxI_, 
+		const ScalingParameters & scales);
 
 	~InelasticCollision();
 
-	const Eigen::VectorXd getGionInterpolated(const Eigen::VectorXd & Te) const;
-	const Eigen::VectorXd getR0ionInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getJ00ionInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getGrecInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getR1recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getR2recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getJ11recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getJ22recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
-	const Eigen::VectorXd getJ12recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getGion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & lambdaIon, const Eigen::VectorXd & TeVec) const;
+	const Eigen::VectorXd getGrec(const double mE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & nI, const Eigen::VectorXd & nE, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & Te, const Eigen::VectorXd & lambdaVec) const;
+	const Eigen::VectorXd getR0ion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaIon) const;
+	const Eigen::VectorXd getJ00ion(const Eigen::VectorXd & nE, const Eigen::VectorXd & nA, const Eigen::VectorXd & vthE, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaIon) const;
+	const Eigen::VectorXd getR1rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const;
+	const Eigen::VectorXd getR2rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const;
+	const Eigen::VectorXd getJ11rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const;
+	const Eigen::VectorXd getJ22rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const;
+	const Eigen::VectorXd getJ12rec(const Eigen::VectorXd & nE, const Eigen::VectorXd & nI, const Eigen::VectorXd & vthE, const Eigen::VectorXd & xStar, const Eigen::VectorXd & TeVec, const Eigen::VectorXd & lambdaRec) const;
 
 	const int getElectronFluidx() const;
 	const int getIonFluidx() const;
 	const int getAtomFluidx() const;
 
+	const double getIonizationEnergyScaled() const;
+
 private:
-	const int idxE;
-	const int idxA;
-	const int idxI;
+	const int idxE = -1;
+	const int idxA = -1;
+	const int idxI = -1;
+
+	double ionizationEnergyScaled = 0;
+
+	int g0 = 0;
+	int g1 = 0;
+	double nScale = 0;
+	double thermalDeBroglieScale = 0;
 
 	Interpolation1d data_Gion;
 	Interpolation2d data_R0ion;
@@ -49,6 +63,15 @@ private:
 	Interpolation2d data_J22rec;
 	Interpolation2d data_J12rec;
 
+	const Eigen::VectorXd getGionInterpolated(const Eigen::VectorXd & Te) const;
+	const Eigen::VectorXd getR0ionInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getJ00ionInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getGrecInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getR1recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getR2recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getJ11recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getJ22recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
+	const Eigen::VectorXd getJ12recInterpolated(const Eigen::VectorXd & Te, const Eigen::VectorXd & lambda) const;
 
 
 };
